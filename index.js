66c39d0652eb60afe048a84e1b413aeb2d1d7a55
Stenio Wagner <stenio.wagner1@gmail.com>
2019-09-16 21:44:49 -0300

    (feat) - extracting and parsing the payload of a request

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const http = require('http');
 
+const getRequestPayload = require('./utils/getRequestPayload');
 const getURLData = require('./utils/getURLData');
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const { method, query, path } = getURLData(req);
 
   res.end('Hello, world!');
@@ -12,6 +13,10 @@ const server = http.createServer((req, res) => {
   console.log('>> Query: ', query);
 
   console.log('>> Method: ', method);
+
+  const payload = await getRequestPayload(req);
+
+  console.log('>> Payload: ', payload, typeof payload);
 });
 
 server.listen(3004, () => {
