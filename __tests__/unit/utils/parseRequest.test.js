718ad355a704ef474bfa65ebc35b529dd712a823
Stenio Wagner <stenio.wagner1@gmail.com>
2019-10-20 23:21:52 -0300

    feat - allowing query array params

--- __tests__/unit/utils/parseRequest.test.js
+++ __tests__/unit/utils/parseRequest.test.js
@@ -13,18 +13,18 @@ const beforeEachTest = (method, url) => {
 };
 
 const parseRequestTest = () => {
-  beforeEachTest('GET', '/users');
+  beforeEachTest('GET', '/users', query = '');
   assert.deepStrictEqual(parseRequest(request), {
     method: HTTP_METHODS.GET,
     path: 'users',
-    query: '',
+    query,
   });
 
   beforeEachTest('GET', '/users/2');
   assert.deepStrictEqual(parseRequest(request), {
     method: HTTP_METHODS.GET,
     path: 'users/2',
-    query: '',
+    query,
   });
 
   beforeEachTest('GET', '/users/2?name=stenio&age=25');
@@ -41,42 +41,71 @@ const parseRequestTest = () => {
   assert.deepStrictEqual(parseRequest(request), {
     method: HTTP_METHODS.POST,
     path: 'users/2',
-    query: '',
+    query,
   });
 
   beforeEachTest('POST', '/users/2/orders');
   assert.deepStrictEqual(parseRequest(request), {
     method: HTTP_METHODS.POST,
     path: 'users/2/orders',
-    query: '',
+    query,
   });
 
   beforeEachTest('PUT', '/users/2');
   assert.deepStrictEqual(parseRequest(request), {
     method: HTTP_METHODS.PUT,
     path: 'users/2',
-    query: '',
+    query,
   });
 
   beforeEachTest('PUT', '/users/2/orders');
   assert.deepStrictEqual(parseRequest(request), {
     method: HTTP_METHODS.PUT,
     path: 'users/2/orders',
-    query: '',
+    query,
   });
 
   beforeEachTest('DELETE', '/users/2');
   assert.deepStrictEqual(parseRequest(request), {
     method: HTTP_METHODS.DELETE,
     path: 'users/2',
-    query: '',
+    query,
   });
 
   beforeEachTest('DELETE', '/users');
   assert.deepStrictEqual(parseRequest(request), {
     method: HTTP_METHODS.DELETE,
     path: 'users',
-    query: '',
+    query,
+  });
+
+  beforeEachTest('GET', '/users?field=value1&field=value2');
+  assert.deepStrictEqual(parseRequest(request), {
+    method: HTTP_METHODS.GET,
+    path: 'users',
+    query: {
+      field: ['value1', 'value2']
+    },
+  });
+
+  beforeEachTest('GET', '/users?field=value1&field=value2&field=value3');
+  assert.deepStrictEqual(parseRequest(request), {
+    method: HTTP_METHODS.GET,
+    path: 'users',
+    query: {
+      field: ['value1', 'value2', 'value3']
+    },
+  });
+
+  beforeEachTest('GET', '/users?other=field&field=value1&another=field&field=value2&field=value3');
+  assert.deepStrictEqual(parseRequest(request), {
+    method: HTTP_METHODS.GET,
+    path: 'users',
+    query: {
+      field: ['value1', 'value2', 'value3'],
+      another: 'field',
+      other: 'field',
+    },
   });
 };
 
--- src/utils/api/parseRequest.js
+++ src/utils/api/parseRequest.js
@@ -6,7 +6,13 @@ const parseQueryParams = query => {
   }
 
   const queryParams = query.split('&').reduce((accumulator, current) => {
-    const [key, value] = current.split('=');
+    let [key, value] = current.split('=');
+
+    if (accumulator[key]) {
+      value = Array.isArray(accumulator[key])
+        ? [...accumulator[key], value]
+        : [accumulator[key], value];
+    }
 
     return ({
       ...accumulator,
